b549d03d7d695a19d8a3d350cb5ed69d895557b8
Sam Aslanowicz <samaslanowicz@sams-air-3.home>
2023-09-11 17:54:02 -0400

    counting multiples of 3 and 5 up to 1000

---
+++ src/Multiples.java
@@ -0,0 +1,18 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int j = 0;
+        for(int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3) {
+
+                j = j + 1;
+
+            } else if (divisibleBy5) {
+
+                j = j + 1;
+            }
+            System.out.println(j);
+        }
+    }
+}
